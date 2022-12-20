3d541008fd9fa953b2f9d350561f253d9b2b9313
bonheurt250 <31155326+bonheurtuyi@users.noreply.github.com>
2022-12-20 21:32:26 +0200

    Organizing getLocation information

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:untitled1/services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
@@ -9,16 +9,17 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  void getLocation() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    debugPrint(location.latitude.toString());
+    debugPrint(location.longitude.toString());
+  }
+
   @override
   void initState() {
     super.initState();
     getLocation();
-  }
-
-  Future<void> getLocation() async {
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.low);
-    debugPrint(position.toString());
   }
 
   @override
